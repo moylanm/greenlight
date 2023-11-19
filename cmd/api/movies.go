babe7d86526e3ee6a03e4f27acb546311ad58f67
Myles Moylan <moylanm@fastmail.com>
2023-11-19 13:13:58 -0800

    Use readJSON helper to decode request body

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -17,7 +17,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		Genres  []string `json:"genres"`
 	}
 
-	err := json.NewDecoder(r.Body).Decode(&input)
+	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 		return
