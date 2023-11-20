ba862d045106954f7191fe92e5596264d84cff52
Myles Moylan <moylanm@fastmail.com>
2023-11-20 12:12:29 -0800

    Fix typo

--- migrations/000001_create_movies_table.up.sql
+++ migrations/000001_create_movies_table.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IS NOT EXISTS movies (
+CREATE TABLE IF NOT EXISTS movies (
 	id bigserial PRIMARY KEY
 	created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
 	title text NOT NULL,
