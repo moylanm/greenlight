bd977c654ca5eb817e44724993ddbb2f4a0903b7
Myles Moylan <moylanm@fastmail.com>
2023-11-20 14:16:17 -0800

    Implement deleteMovieHandler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"golang.org/x/sys/windows/svc/eventlog"
 	"greenlight.mylesmoylan.net/internal/data"
 	"greenlight.mylesmoylan.net/internal/validator"
 )
@@ -126,6 +127,30 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 	}
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	err = app.models.Movies.Delete(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"message": "movie successfully deleted"}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
