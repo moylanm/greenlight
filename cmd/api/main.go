cd241aa129b4ced0ba71fc35d89d5e335717d4e2
Myles Moylan <moylanm@fastmail.com>
2023-11-25 17:43:32 -0800

    Use port 2525 for smtp

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -66,7 +66,7 @@ func main() {
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 
 	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
-	flag.IntVar(&cfg.smtp.port, "smtp-port", 25, "SMTP port")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 2525, "SMTP port")
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "7bb237cef847a7", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "c42c460327ec87", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.mylesmoylan.net>", "SMTP sender")
