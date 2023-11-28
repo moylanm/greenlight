e9a3eae3e44e31b7dfeac5f194e7793abb6b6c87
Myles Moylan <moylanm@fastmail.com>
2023-11-27 17:21:56 -0800

    Update setup script

--- remote/setup/01.sh
+++ remote/setup/01.sh
@@ -5,19 +5,21 @@ set -eu
 # VARIABLES
 # ==================================================================================== #
 
-# Set the timezone for the server.
+# Set the timezone for the server. A full list of available timezones can be found by 
+# running timedatectl list-timezones.
 TIMEZONE=America/Los_Angeles
 
 # Set the name of the new user to create.
 USERNAME=greenlight
 
-# Prompt to enter a password for the PostgreSQL greenlight user.
+# Prompt to enter a password for the PostgreSQL greenlight user (rather than hard-coding
+# a password in this script).
 read -p "Enter password for greenlight DB user: " DB_PASSWORD
 
-# Force all output to be presented in en_US for the duration of this script. This avoids
-# any "setting locale failed" errors while this script is running, before we have
-# installed support for all locales.
-export LC_ALL=en_US.UTF-8
+# Force all output to be presented in en_US for the duration of this script. This avoids  
+# any "setting locale failed" errors while this script is running, before we have 
+# installed support for all locales. Do not change this setting!
+export LC_ALL=en_US.UTF-8 
 
 # ==================================================================================== #
 # SCRIPT LOGIC
@@ -34,7 +36,7 @@ timedatectl set-timezone ${TIMEZONE}
 apt --yes install locales-all
 
 # Add the new user (and give them sudo privileges).
-useradd --create-home --shell "/bin/hash" --groups sudo "${USERNAME}"
+useradd --create-home --shell "/bin/bash" --groups sudo "${USERNAME}"
 
 # Force a password to be set for the new user the first time they log in.
 passwd --delete "${USERNAME}"
@@ -43,7 +45,7 @@ chage --lastday 0 "${USERNAME}"
 # Copy the SSH keys from the root user to the new user.
 rsync --archive --chown=${USERNAME}:${USERNAME} /root/.ssh /home/${USERNAME}
 
-# Configure the firewall to allow SSH, HTTP, and HTTPS traffic.
+# Configure the firewall to allow SSH, HTTP and HTTPS traffic.
 ufw allow 22
 ufw allow 80/tcp
 ufw allow 443/tcp
@@ -53,7 +55,8 @@ ufw --force enable
 apt --yes install fail2ban
 
 # Install the migrate CLI tool.
-curl -L https://github.com/golang-migrate/migrate/releases/download/v4.14.1/migrate.linux-amd64.tar.gz | tar xvz mv migrate.linux-amd64 /usr/local/bin/migrate
+curl -L https://github.com/golang-migrate/migrate/releases/download/v4.14.1/migrate.linux-amd64.tar.gz | tar xvz
+mv migrate.linux-amd64 /usr/local/bin/migrate
 
 # Install PostgreSQL.
 apt --yes install postgresql
@@ -63,18 +66,18 @@ sudo -i -u postgres psql -c "CREATE DATABASE greenlight"
 sudo -i -u postgres psql -d greenlight -c "CREATE EXTENSION IF NOT EXISTS citext"
 sudo -i -u postgres psql -d greenlight -c "CREATE ROLE greenlight WITH LOGIN PASSWORD '${DB_PASSWORD}'"
 
-# Add a DSN for connecting to the greenlight database to the system-wide environment
+# Add a DSN for connecting to the greenlight database to the system-wide environment 
 # variables in the /etc/environment file.
 echo "GREENLIGHT_DB_DSN='postgres://greenlight:${DB_PASSWORD}@localhost/greenlight'" >> /etc/environment
 
-# Install Caddy
+# Install Caddy (see https://caddyserver.com/docs/install#debian-ubuntu-raspbian).
 apt install -y debian-keyring debian-archive-keyring apt-transport-https
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 apt update
 apt --yes install caddy
 
-# Upgrade all packages. Useing the --force-confnew flag means that configuration
+# Upgrade all packages. Using the --force-confnew flag means that configuration 
 # files will be replaced if newer ones are available.
 apt --yes -o Dpkg::Options::="--force-confnew" upgrade
 
